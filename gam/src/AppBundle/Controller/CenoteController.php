884982f52831d55f03cdd5221bcd1f61270c67c7
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-04-13 21:31:25 -0600

    rutas

---
+++ gam/src/AppBundle/Controller/CenoteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class CenoteController extends Controller {
+
+    public function indexAction() {
+
+        echo "ACCION index cenote";
+        die();
+        //put your code here
+    }
+
+}
