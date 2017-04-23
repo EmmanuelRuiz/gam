0ea67fd7e10fd7bbd82058eb2ebd7d1c725660d0
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-04-22 21:23:40 -0600

    diseño
    vamos en la parte de follow del curso, aun que no se lo estoy
    implementando, no es necesario en este caso

--- gam/web/assets/js/custom/users.js
+++ gam/web/assets/js/custom/users.js
@@ -27,48 +27,5 @@ $(document).ready(function(){
 		text: 'No hay más resultados'
 	}));
 	
-	ias.on('ready', function(event){
-		followButton();
-	});
-	
-	ias.on('rendered', function(event){
-		followButton();
-	});
-	
 });
-
-
-//logica para el boton de follow
-function followButton(){
-	$(".btn-follow").unbind("click").click(function(){
-		
-		$(this).addClass("hidden");
-		$(this).parent().find(".btn-unfollow").removeClass("hidden");
-		
-		$.ajax({
-			url: URL+'/follow',
-			type: 'POST',
-			data: {followed: $(this).attr("data-followed")},
-			success: function(response){
-				console.log(response);
-			}
-		});
-	});
-	
-	$(".btn-unfollow").unbind("click").click(function(){
-		
-		$(this).addClass("hidden");
-		$(this).parent().find(".btn-follow").removeClass("hidden");
-		
-		$.ajax({
-			url: URL+'/unfollow',
-			type: 'POST',
-			data: {followed: $(this).attr("data-followed")},
-			success: function(response){
-				console.log(response);
-			}
-		});
-	});
-}
-
 
