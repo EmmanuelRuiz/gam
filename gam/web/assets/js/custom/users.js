fa4097d3217a7b1b8616ec3226a81c89c0aafe2f
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-04-22 20:34:45 -0600

    scroll infinito

---
+++ gam/web/assets/js/custom/users.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+	
+	//alert('entro');
+
+	// Instanciamos el objeto IAS
+	var ias = jQuery.ias({
+		container: '.box-users',
+		item: '.user-item',
+		pagination: '.pagination',
+		next: '.pagination .next_link',
+		triggerPageThreshold: 5
+	});
+	
+	// Boton para ir cargando cada pagina cuando lleguemos a un limite
+	ias.extension(new IASTriggerExtension({
+		text: 'Ver más',
+		offset: 3
+	}));
+	
+	// GIF de efecto cargando
+	ias.extension(new IASSpinnerExtension({
+		src: URL+'/../assets/images/ajax-loader.gif'
+	}));
+	
+	// Mensaje para cuando no existan mas resultados
+	ias.extension(new IASNoneLeftExtension({
+		text: 'No hay más resultados'
+	}));
+	
+	ias.on('ready', function(event){
+		followButton();
+	});
+	
+	ias.on('rendered', function(event){
+		followButton();
+	});
+	
+});
+
+
+//logica para el boton de follow
+function followButton(){
+	$(".btn-follow").unbind("click").click(function(){
+		
+		$(this).addClass("hidden");
+		$(this).parent().find(".btn-unfollow").removeClass("hidden");
+		
+		$.ajax({
+			url: URL+'/follow',
+			type: 'POST',
+			data: {followed: $(this).attr("data-followed")},
+			success: function(response){
+				console.log(response);
+			}
+		});
+	});
+	
+	$(".btn-unfollow").unbind("click").click(function(){
+		
+		$(this).addClass("hidden");
+		$(this).parent().find(".btn-follow").removeClass("hidden");
+		
+		$.ajax({
+			url: URL+'/unfollow',
+			type: 'POST',
+			data: {followed: $(this).attr("data-followed")},
+			success: function(response){
+				console.log(response);
+			}
+		});
+	});
+}
+
+
