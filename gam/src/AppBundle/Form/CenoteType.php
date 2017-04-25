ebdb5ed728f7ad0136e9672a7526469f1f5df019
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-04-24 19:55:22 -0600

    primer formulario de registro hecho

--- gam/src/AppBundle/Form/CenoteType.php
+++ gam/src/AppBundle/Form/CenoteType.php
@@ -66,24 +66,133 @@ class CenoteType extends AbstractType {
                     'attr' => array(
                         'class' => 'form-equipment form-control'
                     )
-                ))                
-                ->add('contextdescription')
-                ->add('relevanttraits')
-                ->add('preservationgeneral')
-                ->add('alteration')
-                ->add('technicaldescription')
-                ->add('depth')
-                ->add('horizontalpenetration')
-                ->add('sitedimensions')
-                ->add('entrancedistance')
-                ->add('siteassociated')
-                ->add('watersamples')
-                ->add('typeofwater')
-                ->add('contact')
-                ->add('exploredby')
-                ->add('documentedby')
-                ->add('inspectiondate')
-                ->add('registrationby')
+                ))               
+                
+                
+                ->add('contextdescription', TextareaType::class, array(
+                    'label' => 'Descripción detallada del sitio (tipo de hallazgo y descripción de contexto) - Site Detailed Description (Kind of Discovery and Context Description) ',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-contextdescription form-control'
+                    )
+                ))
+                ->add('relevanttraits', TextType::class, array(
+                    'label' => 'Rasgo relevantes (Relevant Traits)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-relevanttraits form-control'
+                    )
+                ))
+                ->add('preservationgeneral', TextType::class, array(
+                    'label' => 'Condiciones generales de preservación (Preservation General Conditions) ',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-preservationgeneral form-control'
+                    )
+                ))
+                ->add('alteration', TextareaType::class, array(
+                    'label' => 'En caso de alguna alteración del contexto favor de explicar - (If any context alteration is visible please explain)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-alteration form-control'
+                    )
+                ))
+                
+                
+                
+                ->add('technicaldescription', TextType::class, array(
+                    'label' => 'Descripción técnica de sitio (Site Technical Description)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-technicaldescription form-control'
+                    )
+                ))
+                ->add('depth', TextType::class, array(
+                    'label' => 'Profundidad (Depth)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-depth form-control'
+                    )
+                ))
+                
+                ->add('horizontalpenetration', TextType::class, array(
+                    'label' => 'Penetración horizontal en tiempo (Horizontal Penetration Time)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-horizontalpenetration form-control'
+                    )
+                ))
+                ->add('sitedimensions', TextType::class, array(
+                    'label' => 'Dimensiones del sitio (Site Dimensions)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-sitedimensions form-control'
+                    )
+                ))
+                ->add('entrancedistance', TextType::class, array(
+                    'label' => 'Distancia al ingreso más próximo (Closest Entrance Distance)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-entrancedistance form-control'
+                    )
+                ))
+                ->add('siteassociated', TextType::class, array(
+                    'label' => 'Sitio asociado a (Site associated to)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-siteassociated form-control'
+                    )
+                ))
+                
+                ->add('watersamples', TextType::class, array(
+                    'label' => 'Colecta de muestras de agua (Water Samples Collection)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-watersamples form-control'
+                    )
+                ))
+                ->add('typeofwater', TextType::class, array(
+                    'label' => 'Tipo de agua (Type of water)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-typeofwater form-control'
+                    )
+                ))
+                ->add('contact', TextType::class, array(
+                    'label' => 'Datos de contacto: nombre, número telefónico y/o correo electrónico del dueño/responsable del predio. Contact: (Name, Phone Number and/or email of Owner/Land Responsible)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-contact form-control'
+                    )
+                ))
+                ->add('exploredby', TextType::class, array(
+                    'label' => 'Explorado por: (Explored by:)  ',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-exploredby form-control'
+                    )
+                ))
+                ->add('documentedby', TextType::class, array(
+                    'label' => 'Documentado por: (numero telefónico y/o correo electrónico). Documented by: (Phone Number and / or email) ',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-documentedby form-control'
+                    )
+                ))
+                ->add('inspectiondate', TextType::class, array(
+                    'label' => 'Fecha de registro de sitio (Site Inspection Date) ',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-inspectiondate form-control'
+                    )
+                ))
+                ->add('registrationby', TextType::class, array(
+                    'label' => 'Registro realizado por (Registration by)',
+                    'required' => 'false',
+                    'attr' => array(
+                        'class' => 'form-registrationby form-control'
+                    )
+                ))
                 ->add('sitecode')
                 ->add('coordinates')
                 ->add('state')
