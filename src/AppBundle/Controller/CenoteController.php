eb482a80dc1ac374db46d279e1f19fc8afdcc5ca
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-09-07 21:19:13 -0500

    nada

--- src/AppBundle/Controller/CenoteController.php
+++ src/AppBundle/Controller/CenoteController.php
@@ -17,13 +17,11 @@ class CenoteController extends Controller {
 
     private $session;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->session = new Session();
     }
 
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $dql = "SELECT u FROM BackendBundle:Cenote u ORDER BY u.id ASC";
         $query = $em->createQuery($dql);
@@ -40,8 +38,7 @@ class CenoteController extends Controller {
 
     /* metodo para registrar un cenote */
 
-    public function newCenoteAction(Request $request)
-    {
+    public function newCenoteAction(Request $request) {
 
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
@@ -50,29 +47,21 @@ class CenoteController extends Controller {
 
         //unir la request al objeto que esta vinculado al form
         $form->handleRequest($request);
-        if ($form->isSubmitted())
-        {
-            if ($form->isValid())
-            {
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
 
                 //upload-image
                 $file = $form['image']->getData();
-                if (!empty($file) && $file != null)
-                {
+                if (!empty($file) && $file != null) {
                     $ext = $file->guessExtension();
-                    if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif')
-                    {
+                    if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif') {
                         $file_name = $cenote->getId() . time() . '.' . $ext;
                         $file->move("uploads/cenotes/images", $file_name);
                         $cenote->setImage($file_name);
-                    }
-                    else
-                    {
+                    } else {
                         $cenote->setImage(null);
                     }
-                }
-                else
-                {
+                } else {
                     $cenote->setImage(null);
                 }
 
@@ -82,17 +71,12 @@ class CenoteController extends Controller {
                 $em->persist($cenote);
                 $flush = $em->flush();
 
-                if ($flush == null)
-                {
+                if ($flush == null) {
                     $status = 'El registro se ha creado correctamente';
-                }
-                else
-                {
+                } else {
                     $status = 'Error al intentar hacer el registro';
                 }
-            }
-            else
-            {
+            } else {
                 $status = 'El registro no se ha creado, porque el formulario no es valido.';
             }
             $this->session->getFlashBag()->add("status", $status);
@@ -112,29 +96,24 @@ class CenoteController extends Controller {
       $consulta = $cenotes_repo->findBy(array('user' => $user));
 
       } */
-    public function cenoteAction(Request $request, $id = null)
-    {
+    public function cenoteAction(Request $request, $id = null) {
         $user = $this->getUser();
         $em = $this->getDoctrine()->getManager();
 
         $image_repo = $em->getRepository('BackendBundle:Image')->findAll();
         $video_repo = $em->getRepository('BackendBundle:Video')->findAll();
         $db = $em->getConnection();
-        if ($id != null)
-        {
+        if ($id != null) {
             $cenote_repo = $em->getRepository('BackendBundle:Cenote');
             $cenote = $cenote_repo->findOneBy(array(
                 'id' => $id
             ));
-        }
-        else
-        {
+        } else {
             $cenote = $this->getId();
         }
 
 
-        if (empty($cenote) || !is_object($cenote))
-        {
+        if (empty($cenote) || !is_object($cenote)) {
             return $this->redirect($this->generateUrl('app_homepage'));
         }
 
@@ -145,8 +124,7 @@ class CenoteController extends Controller {
         ));
     }
 
-    public function editCenoteAction(Request $request)
-    {
+    public function editCenoteAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
 
@@ -167,10 +145,8 @@ class CenoteController extends Controller {
 
 
         /* comprobar si el formularion se ha enviado */
-        if ($form->isSubmitted())
-        {
-            if ($form->isValid())
-            {
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
                 $query = $em->createQuery('SELECT u FROM BackendBundle:Cenote u WHERE u.name = :name')
                         ->setParameter('name', $form->get("name")->getData());
 
@@ -178,27 +154,22 @@ class CenoteController extends Controller {
                 $cenote_isset = $query->getResult();
 
                 /* si user_isset es = 0 crea el cenote, si no no se registra por que ya existe */
-                if ((count($cenote_isset) == 0 || $cenote->getName() == $cenote_isset[0]->getName()))
-                {
+                if ((count($cenote_isset) == 0 || $cenote->getName() == $cenote_isset[0]->getName())) {
 
                     // upload archivo
                     $file = $form["image"]->getData();
 
-                    if (!empty($file) && $file != null)
-                    {
+                    if (!empty($file) && $file != null) {
                         // comprobamos que sea un formato de imagen
                         $ext = $file->guessExtension();
-                        if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif')
-                        {
+                        if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif') {
                             // creamos el nombre del archivo nuevo
                             $file_name = $cenote->getId() . time() . '.' . $ext;
                             //carpeta en la que se guardara
                             $file->move("uploads/cenotes/images", $file_name);
                             $cenote->setImage($file_name);
                         }
-                    }
-                    else
-                    {
+                    } else {
                         $cenote->setImage($cenote_image);
                     }
 
@@ -209,22 +180,15 @@ class CenoteController extends Controller {
 
 
                     // mensajes de comprobación 
-                    if ($flush == null)
-                    {
+                    if ($flush == null) {
                         $status = "La información del cenote se a actualizado correctamente";
-                    }
-                    else
-                    {
+                    } else {
                         $status = "No se ha realizado ninguna actualización";
                     }
-                }
-                else
-                {
+                } else {
                     $status = "El cenote ya existe en nuestra base de datos";
                 }
-            }
-            else
-            {
+            } else {
                 $status = "No se ha realizado ninguna actualización";
             }
             $this->session->getFlashBag()->add("status", $status);
@@ -237,8 +201,7 @@ class CenoteController extends Controller {
         ));
     }
 
-    public function uploadImagesAction(Request $request)
-    {
+    public function uploadImagesAction(Request $request) {
 
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
@@ -253,46 +216,33 @@ class CenoteController extends Controller {
         $form = $this->createForm(ImagesType::class, $image);
 
         $form->handleRequest($request);
-        if ($form->isSubmitted())
-        {
-            if ($form->isValid())
-            {
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
                 //upload-image
                 $file = $form['image']->getData();
 
-                if (!empty($file) && $file != null)
-                {
+                if (!empty($file) && $file != null) {
                     $ext = $file->guessExtension();
-                    if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif')
-                    {
+                    if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif') {
                         $file_name = $image->getId() . time() . '.' . $ext;
                         $file->move("uploads/cenotes/images/profile", $file_name);
                         $image->setImage($file_name);
-                    }
-                    else
-                    {
+                    } else {
                         $image->setImage(null);
                     }
-                }
-                else
-                {
+                } else {
                     $image->setImage(null);
                 }
                 $image->setCenote($cenote);
                 $em->persist($image);
                 $flush = $em->flush();
 
-                if ($flush == null)
-                {
+                if ($flush == null) {
                     $status = 'El registro se ha creado correctamente';
-                }
-                else
-                {
+                } else {
                     $status = 'Error al intentar hacer el registro';
                 }
-            }
-            else
-            {
+            } else {
                 $status = 'El registro no se ha creado, porque el formulario no es valido.';
             }
             $this->session->getFlashBag()->add("status", $status);
@@ -305,8 +255,7 @@ class CenoteController extends Controller {
         ));
     }
 
-    public function uploadVideosAction(Request $request)
-    {
+    public function uploadVideosAction(Request $request) {
 
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
@@ -321,49 +270,36 @@ class CenoteController extends Controller {
         $form = $this->createForm(VideosType::class, $video);
 
         $form->handleRequest($request);
-        if ($form->isSubmitted())
-        {
-            if ($form->isValid())
-            {
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
 
                 //upload-image
                 $file = $form['video']->getData();
 
 
-                if (!empty($file) && $file != null)
-                {
+                if (!empty($file) && $file != null) {
                     $ext = $file->guessExtension();
 
-                    if ($ext == 'mp4' || $ext == 'avi' || $ext == 'wmv' || $ext == 'mov')
-                    {
+                    if ($ext == 'mp4' || $ext == 'avi' || $ext == 'wmv' || $ext == 'mov') {
                         $file_name = $video->getId() . time() . '.' . $ext;
                         $file->move("uploads/cenotes/videos/profile", $file_name);
                         $video->setVideo($file_name);
-                    }
-                    else
-                    {
+                    } else {
                         $video->setVideo(null);
                     }
-                }
-                else
-                {
+                } else {
                     $video->setVideo(null);
                 }
                 $video->setCenote($cenote);
                 $em->persist($video);
                 $flush = $em->flush();
 
-                if ($flush == null)
-                {
+                if ($flush == null) {
                     $status = 'El registro se ha creado correctamente';
-                }
-                else
-                {
+                } else {
                     $status = 'Error al intentar hacer el registro';
                 }
-            }
-            else
-            {
+            } else {
                 $status = 'El formulario no es valido';
             }
 
@@ -436,15 +372,13 @@ class CenoteController extends Controller {
       }
      */
 
-    public function searchAction(Request $request)
-    {
+    public function searchAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         //obtenemos el parametro get desde la url
         $search = trim($request->query->get("search", null));
 
         //si no hay nada nos redirige a home
-        if ($search == null)
-        {
+        if ($search == null) {
             return $this->redirect($this->generateUrl('app_homepage'));
         }
 
